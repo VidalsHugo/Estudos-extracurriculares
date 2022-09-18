729f7a33b163802daf6b99f6528a04068fc07951
Hugo Vidal <vidalshugo1@gmail.com>
2022-09-18 17:58:15 -0300

    add ex01

---
+++ javascript/ex01/app.js
@@ -0,0 +1,14 @@
+//ex01
+
+let nomeProduto = "Toddy";
+let precoProduto = 6.99;
+let validadeProduto = true;
+
+
+console.log("Nome do produto: "+ nomeProduto);
+console.log("Preço: "+ precoProduto);
+if(validadeProduto == true){
+    console.log("Produto dentro da validade.");
+}
+
+ 
