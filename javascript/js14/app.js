3c3d4a9b5af998c3d3a57b0bdd0e9eb4aa911638
Hugo Vidal <87623017+VidalsHugo@users.noreply.github.com>
2022-10-12 22:56:31 -0300

    Update app.js

--- javascript/js14/app.js
+++ javascript/js14/app.js
@@ -1,15 +1,15 @@
 function findLongestWordLength(str) {
 
   let soma=0;
-
   let myArray = str.split(" ");
 
-  for(let i=0;i<myArray.length;i++){
-    (myArray[i].length > soma)? soma = myArray[i].length: soma=soma
-  }
-
-
+  myArray.forEach((e) => {
+    (e.length > soma) ? soma=e.length : soma=soma
+  });
+  
   return soma;
 }
 
 console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog"));
+
+// R: 6
