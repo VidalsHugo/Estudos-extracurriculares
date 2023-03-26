b292787734dfd68313908ec808bf78104af9c17d
Hugo Vidal <vidalshugo1@gmail.com>
2023-03-26 15:34:36 -0300

    add csharp4

---
+++ Csdotnet/Calculadora/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Console.WriteLine("Digite a operação: soma, subtracao, divisao ou multiplicacao");
+            var op = Console.ReadLine();
+
+            Console.WriteLine("Digite o primeiro numero: ");
+            double var = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite o segundo numero: ");
+            double varr = Convert.ToDouble(Console.ReadLine());
+            double result=0;
+
+            switch (op)
+            {
+                case "soma":
+                    result = var + varr;
+                    break;
+                case "subtracao":
+                    result = var - varr;
+                    break;
+                case "divisao":
+                    result = var / varr;
+                    break;
+                case "multiplicacao":
+                    result = var * varr;
+                    break;
+
+                default:
+                    Console.WriteLine("ERRO");
+                    break;
+            }
+            Console.WriteLine("resultado: "+result);
+        }
+    }
+}
