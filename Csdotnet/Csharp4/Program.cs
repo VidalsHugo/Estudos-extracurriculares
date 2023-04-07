ddd172321b37e5edaee1084d09db65141d614571
Hugo Vidal <vidalshugo1@gmail.com>
2023-04-06 21:34:32 -0300

    add cs04/05

---
+++ Csdotnet/Csharp4/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Csharp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //guids
+            var id = Guid.NewGuid();
+            Console.WriteLine(id);
+
+            var id2 = Guid.NewGuid();
+            Console.WriteLine(id2);
+
+            if (id == id2)
+            {
+                Console.WriteLine("São iguais!");
+            }
+            else
+            {
+                Console.WriteLine("Não são iguais!");
+            }
+
+            //interpolação
+            var price = 10.2;
+
+            var texto = string.Format("O preço do produto é: {0}.", price);
+            Console.WriteLine(texto);
+
+            var texto2 = $"O preço do produto é: {price}.";
+            Console.WriteLine(texto);
+
+            //Comparação de texto
+            var text = "testando";
+            //verifica se é diferente
+            Console.WriteLine(text.CompareTo("testando"));
+            Console.WriteLine(text.CompareTo("Testajndoo"));
+
+            var text2 = "String teste para testes";
+            Console.WriteLine(text2.Contains("teste"));
+            //ignora o Case sensitive
+            Console.WriteLine(text2.Contains("TeSte", StringComparison.OrdinalIgnoreCase));
+
+            //Verifica se a string é totalmente igual a outra
+            Console.WriteLine(text2.Equals(text));
+
+            Console.WriteLine(text2.IndexOf("para"));
+            Console.WriteLine(text2.LastIndexOf("a"));
+
+        }
+    }
+
+}
---
+++ Csdotnet/Csharp5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Csharp5{
+
+    class Program{
+        static void Main(string[] args){
+
+            var data = new DateTime(); // valor padrao
+            Console.WriteLine(data);
+
+            var data2 = DateTime.Now; //Pega a data e hora atual
+            Console.WriteLine(data2);
+
+            var data3 = new DateTime(2000, 10, 8, 11, 23, 55); // ano, mes, dia, horas, minutos, segundos
+            Console.WriteLine(data3);
+
+            var formatada = String.Format("{0:yyyy/M/d}", data2);
+            Console.WriteLine(formatada);
+
+        }
+    }
+}
