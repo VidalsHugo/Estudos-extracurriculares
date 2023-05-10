d529fe021e47eb9dd16e76bc6bc3de23e31adfc5
Hugo Vidal <vidalshugo1@gmail.com>
2023-05-10 20:31:06 -0300

    add e

---
+++ NodeJS-and-ExpressJS-Course/2-globals.js
@@ -0,0 +1,11 @@
+
+
+console.log(__dirname) // path
+console.log(__filename) //file name
+console.log(require)
+console.log(module)
+console.log(process)
+
+setInterval(()=>{
+    console.log("Hello World")
+}, 1000)
---
+++ NodeJS-and-ExpressJS-Course/3-module.js
@@ -0,0 +1,13 @@
+//modules
+
+const jhon = 'jhon'
+const peter = 'peter'
+
+
+const sayHi = (name) =>{
+    console.log(`Hello there ${name}`);
+}
+
+sayHi("Hugo")
+sayHi(jhon)
+sayHi(peter)
