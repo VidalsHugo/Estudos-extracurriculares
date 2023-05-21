52ee0dd6073f078c43dd21722dcf7a2d6ef3b693
Hugo Vidal <vidalshugo1@gmail.com>
2023-05-21 13:30:07 -0300

    att

--- NodeJS-and-ExpressJS-Course/3-module.js
+++ NodeJS-and-ExpressJS-Course/3-module.js
@@ -3,10 +3,16 @@
 //import
 const names = require('./4-names')
 const sayHi = require('./5-utils')
-
+const data = require('./6-alternative-flavor')
 console.log(names);
 
 sayHi("Hugo")
 sayHi(names.jhon)
 sayHi(names.peter)
+
+console.log(data)
+
+require('./7-mind-grenade')
+
+
 
